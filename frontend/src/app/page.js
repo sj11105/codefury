cc3e5129115314c3a13439dd31ac3db37b3f77e0
sneha <snehajain11105@gmail.com>
2024-08-10 20:56:32 +0530

    sms

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-
-
-import axios from 'axios';
- import Link from 'next/link';
-export default async function Home() {
-   
-    return (
-        <div>
-         <Link href="sms">sms</Link>
-        </div>
-    );
-=======
 
 import Image from "next/image";
 import Navbar from "./components/Navbar";
@@ -23,5 +10,4 @@ export default function Home() {
     
    </div>
   );
->>>>>>> 84f6b24aac0961da6ca93fd16934877c74d053c0
 }
