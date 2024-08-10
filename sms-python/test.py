e5265bd08f77fa801b69a50160d969769e199bad
sneha <snehajain11105@gmail.com>
2024-08-10 12:29:36 +0530

    added the rest

--- sms-python/test.py
+++ sms-python/test.py
@@ -2,23 +2,24 @@ import os
 from dotenv import load_dotenv
 from twilio.rest import Client
 
-# Load environment variables from .env file
+
 load_dotenv()
 
-# Get Twilio credentials from environment variables
+
 account_sid = os.getenv('TWILIO_ACCOUNT_SID')
 auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+twilio_phone_number = os.getenv('TWILIO_PHONE_NUMBER')
 
-# Initialize the Twilio client
+
 client = Client(account_sid, auth_token)
 
-# List of organization phone numbers
+
 recipients = ['+918792067476', '+917889169756']
 
 for recipient in recipients:
     message = client.messages.create(
         body="This is a disaster alert message.",
-        from_='+13867664736',
+        from_=twilio_phone_number,
         to=recipient
     )
     print(f"Message sent to {recipient} with SID: {message.sid}")
